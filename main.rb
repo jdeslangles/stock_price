3cc097a7a469815f8634c909e63a5121dd5fc389
jdeslangles <julien.deslangles@gmail.com>
2013-06-29 10:28:04 +0100

    gets stock price, basic functionality

--- main.rb
+++ main.rb
@@ -4,7 +4,10 @@ require 'sinatra/contrib/all'
 require 'yahoofinance'
 
 get '/'  do
+  if params[:symbol]
+    @symbol = params[:symbol].upcase
+    @quote = YahooFinance.get_standard_quotes(@symbol)[@symbol]
+  end
 
   erb :quote
 end
-
